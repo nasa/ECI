206a76a51cb8457210ae6e3af19dd9107b874e03
cwesterm <christopher.w.westerman@nasa.gov>
2019-07-31 19:59:15 -0400

    FIxed example apps based on 6.6 changes.

--- examples/sgp4Prop/fsw/mission_inc/op_app_perfids.h
+++ examples/sgp4Prop/fsw/mission_inc/op_app_perfids.h
@@ -1,8 +1,6 @@
-
-
 #ifndef OP_APP_PERFIDS_H
 #define OP_APP_PERFIDS_H
 
-
+#define OP_PERF_ID          0x0AB0
 
 #endif
--- examples/sgp4Prop/fsw/platform_inc/op_app_msgids.h
+++ examples/sgp4Prop/fsw/platform_inc/op_app_msgids.h
@@ -1,18 +1,14 @@
-
-
 #ifndef OP_APP_MSGIDS_H
 #define OP_APP_MSGIDS_H
 
-#define OP_FLAG_MID 		0x1800
-#define OP_CMD_MID  		0x1801                  
-#define OP_PERF_ID   		0x1802            
-#define OP_TICK_MID   		0x1803              
-#define OP_HK_MID     		0x1804     
-#define OP_PROPSTATE_MID 	0x1805
+#define OP_CMD_MID          0x1AA0
 
-#define ECI_TBL_MANAGE_MID 	0x1806
-#define ECI_SEND_HK_MID 	0x1807           
+#define OP_FLAG_MID         0x0AA0
+#define OP_TICK_MID         0x0AA1
+#define OP_HK_MID           0x0AA2
+#define OP_PROPSTATE_MID    0x0AA3
 
+#define ECI_TBL_MANAGE_MID  0x0AA4
+#define ECI_SEND_HK_MID     0x0AA5
 
 #endif
-
