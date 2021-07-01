64c60aa5d38da8b0c16aea42c72cf2329a1e8912
oliverhamburger <oliver.hamburger@uvm.edu>
2021-07-01 09:54:59 -0400

    more actions testing

--- actions/build-action/build.sh
+++ actions/build-action/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
 echo "This action is to build ECI"
-sudo apt-get update
-sudo apt-get install gcc-multilib
-. ./ci/fetchCFE.sh
-. ./ci/buildSGP4Example.sh
+../ci/fetchCFE.sh
+../ci/buildSGP4Example.sh
 
