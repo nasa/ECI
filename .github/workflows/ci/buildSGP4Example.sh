62c18ded08783317e2afca74f699937878f279ad
oliverhamburger <oliver.hamburger@uvm.edu>
2021-07-07 15:51:53 -0400

    updated ci scripts to build with bootes-cv2

--- .github/workflows/ci/buildSGP4Example.sh
+++ .github/workflows/ci/buildSGP4Example.sh
@@ -8,35 +8,32 @@ if [[ "$CI" == true ]]; then
 fi
 
 # copy app files to CFS apps dir
-mkdir ./cfs/apps/op
-cp -r ./examples/sgp4Prop/* ./cfs/apps/op/
+mkdir ./cFS/apps/op
+cp -r ./examples/sgp4Prop/* ./cFS/apps/op/
 # copy eci source code to CFS apps dir 
-mkdir ./cfs/apps/eci
-mkdir ./cfs/apps/eci/fsw
-cp -r ./fsw/* ./cfs/apps/eci/fsw/
+mkdir ./cFS/apps/eci
+mkdir ./cFS/apps/eci/fsw
+cp -r ./fsw/* ./cFS/apps/eci/fsw/
 # debugging
-ls ./cfs/apps/op/
+ls ./cFS/apps/op/
 # setup environment for compiling
-cd ./cfs
+cd ./cFS
 
 # make any code changes needed to integrate this app
-# ensure CFS builds new app we added
-sed -i '44a THE_APPS += op' ./build/cpu1/Makefile
-sed -i '50a THE_TBLS += op' ./build/cpu1/Makefile
+# add op to applist(bootes)
+sed -i '106a SET(TGT1_APPLIST ci_lab to_lab sch_lab op)'./sample_defs/targets.cmake
 # configure the app to run when CFS starts
-sed -i '5a CFE_APP, /cf/op.so,          op_AppMain,     OP_APP,       90,   8192, 0x0, 0;' ./build/cpu1/exe/cfe_es_startup.scr
+sed -i '6a CFE_APP, /cf/op.so,          op_AppMain,     OP_APP,       90,   8192, 0x0, 0;' ./sample_defs/cpu1_cfe_es_startup.scr
 sed -i '26a #include "op_app_msgids.h"' ./apps/sch_lab/fsw/platform_inc/sch_lab_sched_tab.h
 sed -i '74a      { OP_TICK_MID,   1, 0 },' ./apps/sch_lab/fsw/platform_inc/sch_lab_sched_tab.h
-# update makefile to include math library
+# update makefile to include math library(check and make sure this file still exists)
 sed -i '42s/.*/\t$(COMPILER) -m32 -shared -o $@ $(OBJS) -lm/' ./psp/fsw/pc-linux/make/link-rules.mak
 # Note: this should be a temporary fix until its determined how to
 # add a library via a supported mechanism
 
 # prepare environment
-. ./setvars.sh
 cd ./build/cpu1
 
 # compile
-make clean
-make config
+make prep
 make
