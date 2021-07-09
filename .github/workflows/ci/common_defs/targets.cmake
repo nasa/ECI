05079c4db6b4022992bc46bfa01ced2f0d277520
oliverhamburger <oliver.hamburger@uvm.edu>
2021-07-09 17:23:20 -0400

    sgp4 build fix

--- .github/workflows/ci/common_defs/targets.cmake
+++ .github/workflows/ci/common_defs/targets.cmake
@@ -102,7 +102,7 @@ SET(FT_INSTALL_SUBDIR "host/functional-test")
 SET(MISSION_CPUNAMES cpu1)
 
 SET(cpu1_PROCESSORID 1)
-SET(cpu1_APPLIST ci_lab to_lab sch_lab simpleECIApp)
+SET(cpu1_APPLIST ci_lab to_lab sch_lab)
 SET(cpu1_FILELIST cfe_es_startup.scr)
 
 # CPU2 example.  This is not built by default anymore but
