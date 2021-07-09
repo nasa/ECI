494ae7f6d609c6873b4dd71ade4bdde550eae7cb
oliverhamburger <oliver.hamburger@uvm.edu>
2021-07-09 01:01:34 -0400

    path fix

--- .github/workflows/ci/buildSimpleExample.sh
+++ .github/workflows/ci/buildSimpleExample.sh
@@ -18,7 +18,7 @@ cp -r ./fsw/* ./cFS/apps/eci/fsw/
 cd ./cFS
 # ensure CFS builds new app we added
 sed -i '105a SET(cpu1_APPLIST ci_lab to_lab sch_lab simpleECIApp)' ./sample_defs/targets.cmake
-sed -i '6a CFE_APP, simpleECIApp,sa_AppMain,         SA_APP,       90,   8192,  0x0, 0;' ./sample_defs/cpu1_es_startup.scr
+sed -i '6a CFE_APP, simpleECIApp,sa_AppMain,         SA_APP,       90,   8192,  0x0, 0;' ./sample_defs/cpu1_cfe_es_startup.scr
 
 # compile
 make SIMULATION=native prep
