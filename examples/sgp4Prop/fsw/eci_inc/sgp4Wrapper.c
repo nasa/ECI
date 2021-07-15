b2c5b5539271a0a17e8291e53c0f838e93c89e18
oliverhamburger <oliver.hamburger@uvm.edu>
2021-07-15 16:53:18 -0400

    Fix #57, Add clang-format and clang-format gihub action

--- examples/sgp4Prop/fsw/eci_inc/sgp4Wrapper.c
+++ examples/sgp4Prop/fsw/eci_inc/sgp4Wrapper.c
@@ -5,12 +5,12 @@
 
 /* Declare variables with global scope so ECI can access them */
 /* Inputs */
-tle_lines_t *          tle_lines;
+tle_lines_t *tle_lines;
 extern ECI_TimeStamp_t ECI_Step_TimeStamp;
 
 /* Internal */
-TLE          tle;
-double       mins = 0;
+TLE tle;
+double mins = 0;
 line_check_t line1Check;
 line_check_t line2Check;
 
@@ -20,127 +20,122 @@ propState_t propState;
 /* The SGP4 implementation used in this example is not suitable
  * for implementation via ECI because the top-level main function
  * in the implementation uses unavailable APIs (file reading) during
- * the call stack. Here we define a wrapper function to call the
- * specific functionality needed for the propagation as a step
+ * the call stack. Here we define a wrapper function to call the 
+ * specific functionality needed for the propagation as a step 
  * function for ECI to call.
  */
-void propagate(void)
-{
+void propagate(void){
     /* Get the current time from FSW */
-    mins = ((double)ECI_Step_TimeStamp.Seconds + ((double)ECI_Step_TimeStamp.Subseconds / 4294967296.0)) / 60.0;
+    mins = ((double)ECI_Step_TimeStamp.Seconds + ((double)
+        ECI_Step_TimeStamp.Subseconds/4294967296.0)) / 60.0;
 
     propState.t = mins;
-
+    
     propState.invalid = false;
 
-    /* This doesn't need to be re-processed each cycle, only
-     * when the TLE table is updated, but we but need some way
-     * to determine the table has changed in order to not
+    /* This doesn't need to be re-processed each cycle, only 
+     * when the TLE table is updated, but we but need some way 
+     * to determine the table has changed in order to not 
      * reprocess every cycle.
      */
     parseLines(&tle, tle_lines->line1, tle_lines->line2);
 
     /* Do the propagation */
     getRV(&tle, mins, propState.r, propState.v);
-
+    
     /* Check to see if current FSW time is outside range of the TLE */
-    if (mins < tle.epoch)
-    {
-        propState.invalid = true;
+    if(mins < tle.epoch){
+	propState.invalid = true;
     }
+
 
     /* The results are stored in outData which ECI will emit
      * onto the software bus for downstream use.
      */
+
 }
 
 /* Begin implementation of table validation */
 
-int digitToInt(char digit)
-{
-    /* Convert a character containing a digit to the numerical
-     * equivalent.
-     *
-     * Note: This return incorrect results if the character
-     *     to convert is not a digit. Call isdigit() to check
-     *     input before calling digitToInt()!
-     */
-
+int digitToInt(char digit){
+/* Convert a character containing a digit to the numerical
+ * equivalent. 
+ *
+ * Note: This return incorrect results if the character 
+ *     to convert is not a digit. Call isdigit() to check
+ *     input before calling digitToInt()!
+ */
+    
     /* C gaurantees that digits 0-9 are stored contiguously
      * so we can subtract the character '0' to convert a
      * character to int*/
     return digit - '0';
 }
 
-int computeTLEChecksum(char line[70])
-{
-    /* Computes a checksum for a TLE line.
+int computeTLEChecksum(char line[70]){
+/* Computes a checksum for a TLE line. 
 
-     * Details of the algorithm are included in comments
-     * below and were implemented from the algorithm
-     * description found here:
-     * http://www.stltracker.com/resources/tle
-     */
-
+ * Details of the algorithm are included in comments 
+ * below and were implemented from the algorithm 
+ * description found here:
+ * http://www.stltracker.com/resources/tle
+ */    
+    
     /* set checksum to 0 */
     int checksum = 0;
 
     /* for character from 1 to 68 */
-    for (int i = 0; i < 68; i++)
-    {
-
-        /* if i'th character in the line is a digit */
-        if (isdigit(line[i]))
-        {
-            /* add value of i'th character in the line to checksum*/
-            checksum += digitToInt(line[i]);
-        }
-        /* else if i'th character in the line is '-' */
-        else if (line[i] == '-')
-        {
-            /* add 1 to checksum */
-            checksum++;
-        }
+    for (int i = 0; i < 68; i++){
+        
+      /* if i'th character in the line is a digit */
+      if (isdigit(line[i])){
+        /* add value of i'th character in the line to checksum*/
+        checksum += digitToInt(line[i]);
+      }
+      /* else if i'th character in the line is '-' */
+      else if (line[i] == '-'){
+        /* add 1 to checksum */
+        checksum++;
+      }
     }
 
     /* set checksum to the value of the checksum modulo 10 */
     return checksum % 10;
 }
 
-int32_t validateTLE(tle_lines_t *TblPtr)
-{
-    /* Validates that each of the TLE lines match their built-in
-     * checksums.
-     *
-     * Note: Table validation function must have the following signature:
-            int32 validateFcn(void * TblPtr);
-     * and return a negative value if the table should fail validation.
-     */
-
+int32_t validateTLE(tle_lines_t * TblPtr){
+/* Validates that each of the TLE lines match their built-in 
+ * checksums.
+ *
+ * Note: Table validation function must have the following signature:
+        int32 validateFcn(void * TblPtr);
+ * and return a negative value if the table should fail validation.
+ */
+ 
     /* Initialize checks */
     line1Check.computed = computeTLEChecksum(TblPtr->line1);
     line1Check.expected = digitToInt(TblPtr->line1[68]);
-    line1Check.failed   = line1Check.computed != line1Check.expected;
+    line1Check.failed =  line1Check.computed != line1Check.expected;
 
     /* Each line is validated independently */
     line2Check.computed = computeTLEChecksum(TblPtr->line2);
     line2Check.expected = digitToInt(TblPtr->line2[68]);
-    line2Check.failed   = line2Check.computed != line2Check.expected;
+    line2Check.failed =  line2Check.computed != line2Check.expected;
 
     /* Additional validation of the format of the TLE could also be
      * done, but is not relevant to this example.
      */
-
-    /* Each passed flag is a boolean value which is cast to either
-     * 0 (if the checksum validated) or 1 (if it didn't). By
-     * subtracting 1 from each flag, we convert each value to a
+        
+    /* Each passed flag is a boolean value which is cast to either 
+     * 0 (if the checksum validated) or 1 (if it didn't). By 
+     * subtracting 1 from each flag, we convert each value to a 
      * negative value (if it failed) or zero if it passed. By adding
      * the two values, we get:
-     *       0 if both passed
+     *       0 if both passed 
      *      -1 if one line failed
      *      -2 if both lines failed
-     * which meets the requirement of a negative return value to
+     * which meets the requirement of a negative return value to 
      * fail the table.
      */
-    return (-(int32_t)line1Check.failed) + (-(int32_t)line2Check.failed);
+    return (-(int32_t) line1Check.failed) + (-(int32_t) line2Check.failed);
 }
--- examples/sgp4Prop/fsw/eci_inc/sgp4Wrapper.h
+++ examples/sgp4Prop/fsw/eci_inc/sgp4Wrapper.h
@@ -10,36 +10,33 @@
 #include <stdint.h>
 
 /* Define types */
-typedef struct
-{
-    uint8_t header[12]; /* Reserve space for packet header */
-    double  r[3];
-    double  v[3];
-    double  t;
-    bool    invalid;
+typedef struct  {
+	uint8_t header[12]; /* Reserve space for packet header */
+    double r[3];
+    double v[3];
+    double t;
+    bool invalid;
 } propState_t;
 
-typedef struct
-{
+typedef struct  {
     char line1[70];
     char line2[70];
 } tle_lines_t;
 
-typedef struct
-{
+typedef struct  {
     bool failed;
-    int  expected;
-    int  computed;
+    int expected;
+    int computed;
 } line_check_t;
 
 /* Declare extern variables for ECI*/
-extern propState_t  propState;
-extern TLE          tle;
+extern propState_t propState;
+extern TLE tle;
 extern tle_lines_t *tle_lines;
-extern bool         checksum1Passed, checksum2Passed;
+extern bool checksum1Passed, checksum2Passed;
 
 /* Function prototypes for ECI use */
-void    propagate(void);
-int32_t validateTLE(tle_lines_t *TblPtr);
+void propagate(void);
+int32_t validateTLE(tle_lines_t * TblPtr);
 
 #endif
