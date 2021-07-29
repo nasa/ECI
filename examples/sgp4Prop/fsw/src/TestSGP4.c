eda8ea62e4dc2d64b0a174e52869d7f22ac44b03
oliverhamburger <oliver.hamburger@uvm.edu>
2021-07-29 18:55:34 -0400

    un-formated third part code

--- examples/sgp4Prop/fsw/src/TestSGP4.c
+++ examples/sgp4Prop/fsw/src/TestSGP4.c
@@ -4,10 +4,9 @@
 #include <math.h>
 #include "TLE.h"
 
-typedef struct VERIN
-{
-    char   line1[70];
-    char   line2[70];
+typedef struct VERIN {
+    char line1[70];
+    char line2[70];
     double startmin;
     double stepmin;
     double stopmin;
@@ -18,47 +17,47 @@ typedef struct VERIN
  */
 int readVERINs(VERIN **listptr)
 {
-    char   line[256];
-    char * str     = NULL;
-    FILE * in_file = NULL;
-    VERIN *verins  = NULL;
-    int    cnt     = 0;
+    char line[256];
+    char *str = NULL;
+    FILE *in_file = NULL;
+    VERIN *verins = NULL;
+    int cnt = 0;
 
-    in_file = fopen("../../data/SGP4-VER.TLE", "r");
 
-    while (fgets(line, 255, in_file) != NULL)
+    in_file = fopen("../../data/SGP4-VER.TLE","r");
+    
+    while(fgets(line,255,in_file) != NULL)
     {
-        if (line[0] == '1')
-            cnt++;
+        if(line[0]=='1')cnt++;
     }
 
-    if (in_file)
+    if(in_file)
     {
         fclose(in_file);
     }
 
-    verins   = malloc(cnt * sizeof(VERIN));
+    verins = malloc(cnt*sizeof(VERIN));
     *listptr = verins;
 
-    cnt     = 0;
-    in_file = fopen("../../data/SGP4-VER.TLE", "r");
-
-    while (fgets(line, 255, in_file) != NULL)
+    cnt = 0;
+    in_file = fopen("../../data/SGP4-VER.TLE","r");
+    
+    while(fgets(line,255,in_file) != NULL)
     {
-        if (line[0] == '1')
+        if(line[0]=='1')
         {
-            strncpy(verins[cnt].line1, line, 69);
-            verins[cnt].line1[69] = 0;
-            fgets(line, 255, in_file);
-            strncpy(verins[cnt].line2, line, 69);
-            verins[cnt].line2[69] = 0;
-            str                   = &line[70];
-            sscanf(str, "%lf %lf %lf", &verins[cnt].startmin, &verins[cnt].stopmin, &verins[cnt].stepmin);
+            strncpy(verins[cnt].line1,line,69);
+            verins[cnt].line1[69]=0;
+            fgets(line,255,in_file);
+            strncpy(verins[cnt].line2,line,69);
+            verins[cnt].line2[69]=0;
+            str = &line[70];
+            sscanf(str,"%lf %lf %lf",&verins[cnt].startmin,&verins[cnt].stopmin,&verins[cnt].stepmin);
             cnt++;
         }
     }
 
-    if (in_file)
+    if(in_file)
     {
         fclose(in_file);
     }
@@ -72,69 +71,69 @@ int readVERINs(VERIN **listptr)
 double dist(double *v1, double *v2)
 {
     double dist = 0;
-    double tmp  = 0;
+    double tmp = 0;
 
     // just unroll the loop
-    tmp = v1[0] - v2[0];
-    dist += tmp * tmp;
-    tmp = v1[1] - v2[1];
-    dist += tmp * tmp;
-    tmp = v1[2] - v2[2];
-    dist += tmp * tmp;
+    tmp = v1[0]-v2[0];
+    dist += tmp*tmp;
+    tmp = v1[1]-v2[1];
+    dist += tmp*tmp;
+    tmp = v1[2]-v2[2];
+    dist += tmp*tmp;
 
     return sqrt(dist);
 }
 
 void runVER(VERIN *verins, int cnt)
 {
-    FILE * in_file = NULL;
-    TLE    tle;
+    FILE *in_file = NULL;
+    TLE tle;
     double r[3];
     double v[3];
     double rv[3];
     double vv[3];
     double mins = 0;
-    char   line[256];
-    int    i     = -1;
-    char * ind   = NULL;
+    char line[256];
+    int i=-1;
+    char *ind = NULL;
     double rdist = 0;
     double vdist = 0;
-    double rerr  = 0;
-    double verr  = 0;
-    int    cnt2  = 0;
+    double rerr = 0;
+    double verr = 0;
+    int cnt2 = 0;
 
-    in_file = fopen("../../data/tcppver.out", "r");
+    in_file = fopen("../../data/tcppver.out","r");
 
-    while (fgets(line, 255, in_file) != NULL && i < cnt)
+    while(fgets(line,255,in_file) != NULL && i <cnt)
     {
-        ind = strstr(line, "xx");
-        if (ind != NULL)
+        ind = strstr(line,"xx");
+        if(ind != NULL)
         {
             i++;
-            parseLines(&tle, verins[i].line1, verins[i].line2);
-        }
+            parseLines(&tle,verins[i].line1,verins[i].line2);
+        } 
         else
         {
-            sscanf(line, "%lf %lf %lf %lf %lf %lf %lf\n", &mins, rv, &rv[1], &rv[2], vv, &vv[1], &vv[2]);
-            getRV(&tle, mins, r, v);
-            rdist = dist(r, rv);
-            vdist = dist(v, vv);
+            sscanf(line,"%lf %lf %lf %lf %lf %lf %lf\n",&mins,rv,&rv[1],&rv[2],vv,&vv[1],&vv[2]);
+            getRV(&tle,mins,r,v);
+            rdist = dist(r,rv);
+            vdist = dist(v,vv);
             rerr += rdist;
             verr += vdist;
             cnt2++;
-            if (rdist > 1e-7 || vdist > 1e-8)
+            if(rdist > 1e-7 || vdist > 1e-8)
             {
-                printf("%d %lf %e %e\n", tle.objectNum, mins, rdist, vdist);
+                printf("%d %lf %e %e\n",tle.objectNum,mins,rdist,vdist);
             }
         }
     }
 
-    rerr = rerr / cnt2;
-    verr = verr / cnt2;
+    rerr = rerr/cnt2;
+    verr = verr/cnt2;
 
-    printf("Typical errors r=%e mm, v=%e mm/s\n", 1e6 * rerr, 1e6 * verr);
+    printf("Typical errors r=%e mm, v=%e mm/s\n",1e6*rerr,1e6*verr);
 
-    if (in_file)
+    if(in_file)
     {
         fclose(in_file);
     }
